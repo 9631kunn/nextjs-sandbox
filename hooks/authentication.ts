dd7b8e7811da46830b4c121019dc17456e8a3a71
9631kunn <infourbh+1@gmail.com>
2021-03-31 18:19:59 +0900

    FireStoreへの書き込み

--- hooks/authentication.ts
+++ hooks/authentication.ts
@@ -1,41 +1,56 @@
-import {useEffect} from "react"
+import { useEffect } from "react";
 
-import firebase from "firebase/app"
-import { User } from "../models/User"
-import {atom, useRecoilState} from "recoil"
+import firebase from "firebase/app";
+import { User } from "../models/User";
+import { atom, useRecoilState } from "recoil";
 
 const userState = atom<User>({
   key: "user",
-  default: null
-})
+  default: null,
+});
+
+async function createUserIfNotFound(user: User) {
+  const userRef = firebase.firestore().collection("users").doc(user.uid);
+  const doc = await userRef.get();
+  if (doc.exists) {
+    return;
+  }
+  await userRef.set({
+    name: "名無し" + new Date().getTime(),
+  });
+}
 
 export function useAuthentication() {
-  const [user, setUser] = useRecoilState(userState)
+  const [user, setUser] = useRecoilState(userState);
 
   // 最初のアクセス時判定
   useEffect(() => {
     if (user !== null) {
-      return
+      return;
     }
 
-    firebase.auth()
-      .signInAnonymously().catch(function (error) {
-        console.error(error)
-      })
+    firebase
+      .auth()
+      .signInAnonymously()
+      .catch(function (error) {
+        console.error(error);
+      });
 
     firebase.auth().onAuthStateChanged(function (firebaseUser) {
       if (firebaseUser) {
         // ログイン
-        setUser({
+        const loginUser: User = {
           uid: firebaseUser.uid,
-          isAnonymous: firebaseUser.isAnonymous
-        })
+          isAnonymous: firebaseUser.isAnonymous,
+        };
+        setUser(loginUser);
+        createUserIfNotFound(loginUser);
       } else {
         // 未ログイン
-        setUser(null)
+        setUser(null);
       }
-    })
-  }, [])
+    });
+  }, []);
 
-  return {user}
+  return { user };
 }
